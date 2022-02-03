588ba59f09d982f7beb5e3a78ab7fa15b516c452
dP <dp@dpointer.org>
2022-02-03 04:25:13 +0300

    Missing files

---
+++ grf/sprites.py
@@ -0,0 +1,177 @@
+import struct
+import numpy as np
+from PIL import Image
+
+from .grf import BaseSprite, ZOOM_4X, convert_image, BPP_8, BPP_24, RealSprite, fix_palette
+
+
+class NMLFileSpriteWrapper:
+    def __init__(self, sprite, file, bit_depth):
+        self.sprite = sprite
+        self.file = type('Filename', (object, ), {'value': file.path})
+        self.bit_depth = bit_depth
+        self.mask_file = type('Filename', (object, ), {'value': file.mask}) if file.mask else None
+        self.mask_pos = None
+        self.flags = type('Flags', (object, ), {'value': 0})
+
+    xpos = property(lambda self: type('XSize', (object, ), {'value': self.sprite.x}))
+    ypos = property(lambda self: type('YSize', (object, ), {'value': self.sprite.y}))
+
+    xsize = property(lambda self: type('XSize', (object, ), {'value': self.sprite.w}))
+    ysize = property(lambda self: type('YSize', (object, ), {'value': self.sprite.h}))
+
+    xrel = property(lambda self: type('XSize', (object, ), {'value': self.sprite.xofs}))
+    yrel = property(lambda self: type('YSize', (object, ), {'value': self.sprite.yofs}))
+
+
+class BaseImageSprite(RealSprite):
+    def __init__(self, x, y, w, h, *, bpp=None, mask=None, **kw):
+        if bpp == BPP_8 and mask is not None:
+            raise ValueError("8bpp sprites can't have a mask")
+        super().__init__(w, h, **kw)
+        self.bpp = bpp
+        self.mask = mask
+        self.x = x
+        self.y = y
+        self.name = f'{self.x},{self.y} {self.w}x{self.h}'
+
+    def get_image(self):
+        raise NotImplementedError
+
+    def get_mask_image(self):
+        raise NotImplementedError
+
+    def get_colourkey(self):
+        return None
+
+    # https://github.com/OpenTTD/grfcodec/blob/master/docs/grf.txt
+    def get_real_data(self, encoder):
+        img, bpp = self.get_image()
+        if self.bpp is None:
+            self.bpp = bpp
+        npalpha = None
+
+        if bpp != self.bpp:
+            print(f'Sprite {self.name} expected {self.bpp}bpp but file is {bpp}bpp, converting.')
+            if bpp == BPP_8:
+                img = img.convert('RGB')
+            else:
+                p_img = Image.new('P', (16, 16))
+                p_img.putpalette(PALETTE)
+                img = img.quantize(palette=p_img, dither=0)
+        else:
+            if bpp == BPP_8:
+                img = fix_palette(img, self.name)
+
+        npimg = np.asarray(img)
+        colourkey = self.get_colourkey()
+        if colourkey is not None:
+            if self.bpp == BPP_24 and colourkey is not None:
+                npalpha = 255 * np.all(np.not_equal(npimg, colourkey), axis=2)
+                npalpha = npalpha.astype(np.uint8, copy=False)
+            else:
+                print(f'Colour key on 8bpp sprites is not supported')
+
+        info_byte = 0x40
+        if self.bpp == BPP_24:
+            npimg.shape = (self.w * self.h, 3)
+            stack = [npimg]
+            info_byte |= 0x1  # rgb
+            rbpp = 3
+
+            if npalpha is not None:
+                npalpha.shape = (self.w * self.h, 1)
+                stack.append(npalpha)
+                rbpp += 1
+                info_byte |= 0x2  # alpha
+
+            if self.mask is not None:
+                mask_file, xofs, yofs = self.mask
+                mask_img, mask_bpp = self.get_mask_image()
+                if mask_bpp != BPP_8:
+                    raise RuntimeError(f'Mask {mask_file.path} is not an 8bpp image')
+                xofs, yofs = self.x + xofs, self.y + yofs
+                mask_img = mask_img.crop((xofs, yofs, xofs + self.w, yofs + self.h))
+                mask_img = fix_palette(mask_img, f'{mask_file.path} {xofs},{yofs} {self.w}x{self.h}')
+                npmask = np.asarray(mask_img)
+                npmask.shape = (self.w * self.h, 1)
+                stack.append(npmask)
+                rbpp += 1
+                info_byte |= 0x4  # mask
+
+            if len(stack) > 1:
+                raw_data = np.concatenate(stack, axis=1)
+            else:
+                raw_data = stack[0]
+            raw_data.shape = self.w * self.h * rbpp
+        else:
+            info_byte |= 0x4  # pal/mask
+            raw_data = npimg
+            raw_data.shape = self.w * self.h
+
+        data = encoder.sprite_compress(raw_data)
+        return struct.pack(
+            '<IIBBHHhh',
+            self.sprite_id,
+            len(data) + 10, info_byte,
+            self.zoom,
+            self.h,
+            self.w,
+            self.xofs,
+            self.yofs,
+        ) + data
+
+
+class ImageSprite(BaseImageSprite):
+    def __init__(self, image, x, y, w, h, *, mask=None, **kw):
+        self._image = convert_image(image)
+        if mask:
+            mask_image, xofs, yofs = mask
+            mask = convert_image(mask_image), xofs, yofs
+        super().__init__(x, y, w, h, bpp=self._image[1], mask=mask, **kw)
+
+    def get_image(self):
+        return self._image
+
+    def get_mask_image(self):
+        return self.mask[0] if self.mask else None
+
+
+class ImageFile:
+    def __init__(self, path, colourkey=None):
+        self.path = path
+        self.colourkey = colourkey
+        self._image = None
+
+    def get_image(self):
+        if self._image:
+            return self._image
+        img = Image.open(self.path)
+        # TODO alpha channel
+        if img.mode == 'P':
+            self._image = (img, BPP_8)
+        else:
+            if img.mode != 'RGB':
+                img = img.convert('RGB')
+            self._image = (img, BPP_24)
+        return self._image
+
+
+class FileSprite(BaseImageSprite):
+    def __init__(self, file, x, y, w, h, *, bpp=None, mask=None, **kw):
+        assert(isinstance(file, ImageFile))
+        super().__init__(x, y, w, h, bpp=bpp, mask=mask, **kw)
+        self.file = file
+
+    def get_image(self):
+        img, bpp = self.file.get_image()
+        img = img.crop((self.x, self.y, self.x + self.w, self.y + self.h))
+        return img, bpp
+
+    def get_mask_image(self):
+        mask_file, _, _ = self.mask
+        return mask_file.get_image()
+
+    def get_colourkey(self):
+        return self.file.colourkey
+
---
+++ grf/vox.py
@@ -0,0 +1,195 @@
+import struct
+import numpy as np
+from PIL import Image
+
+from .sprites import ImageSprite
+from .grf import convert_image, find_best_color, to_spectra, PaletteRemap, ZOOM_4X, ZOOM_2X, ZOOM_NORMAL, PALETTE
+
+
+VOX_SIDE_RIGHT, VOX_SIDE_BOTTOM = 0x100, 0x200
+VOX_SIDE_SHIFT, VOX_SIDE_MASK = 8, VOX_SIDE_RIGHT | VOX_SIDE_BOTTOM
+VOX_SIDE_ZL, VOX_SIDE_ZR, VOX_SIDE_X, VOX_SIDE_Y = 0, VOX_SIDE_RIGHT, VOX_SIDE_BOTTOM, VOX_SIDE_RIGHT | VOX_SIDE_BOTTOM
+
+
+class VoxReader:
+    def __init__(self, path):
+        self.path = path
+        self.size = None
+        self.voxels = []
+        self.palette = None
+
+    def _read_size(self, f):
+        s = struct.unpack('<III', f.read(12))
+        self.size = [s[1], s[0], s[2]]
+
+    def _read_xyzi(self, f):
+        n = struct.unpack('<I', f.read(4))[0]
+        self.voxels = np.fromfile(f, count=n * 4, dtype=np.uint8)
+        self.voxels.shape = (n, 4)
+
+        # x, y, z, i = sx - y, x, z, i
+        self.voxels[:, [0, 1]] = self.voxels[:, [1, 0]]
+        self.voxels[:, 0] = -self.voxels[:, 0]
+        self.voxels = np.array((self.size[0], 0, 0, 0)) + self.voxels
+
+    def _read_rgba(self, f):
+        vox_palette = np.fromfile(f, count=256 * 4, dtype=np.uint8)
+        vox_palette.shape = (256, 4)
+        self.palette = [find_best_color(to_spectra(r, g, b)) for r, g, b, _ in vox_palette]
+
+    def read(self):
+        with open(self.path, 'rb') as f:
+            f.read(4)  # VOX
+            f.read(4)  # Version
+            while True:
+                chunk_id = f.read(4)
+                if not chunk_id:
+                    break
+                n, m = struct.unpack('<II', f.read(8))
+                pos = f.tell()
+                if chunk_id == b'XYZI':
+                    self._read_xyzi(f)
+                if chunk_id == b'SIZE':
+                    self._read_size(f)
+                elif chunk_id == b'RGBA':
+                    self._read_rgba(f)
+                f.seek(pos + n, 0)
+
+
+class VoxFile:
+    def __init__(self, path, *,
+                 x_colour_func=lambda c: c.darken(20),
+                 y_colour_func=None,
+                 z_colour_func=lambda c: c.brighten(10)):
+        if isinstance(path, VoxReader):
+            self.reader = path
+            self.path = None
+        else:
+            self.path = path
+            self.reader = VoxReader(path)
+
+        self.x_colour_func = x_colour_func
+        self.y_colour_func = y_colour_func
+        self.z_colour_func = z_colour_func
+
+        self.data = None
+
+    def get_image(self):
+        pass
+
+    def _load(self):
+        self.reader.read()
+        cam_norm = np.array((6**.5 / 4., 6**.5 / 4., .5, 0))
+        xx = self.reader.voxels @ np.array((1, -1, 0, 0)).T
+        yy = self.reader.voxels @ np.array((1, 1, -2, 0)).T
+        zz = self.reader.voxels @ cam_norm
+        cc = np.take(self.reader.palette, self.reader.voxels[:, 3])
+        xmin, xmax = np.amin(xx), np.amax(xx)
+        ymin, ymax = np.amin(yy), np.amax(yy)
+        self.w = xmax - xmin + 2
+        self.h = ymax - ymin + 3
+        zbuf = np.full((self.h, self.w), -1e100)
+        data = np.zeros((self.h, self.w), dtype=np.uint16)
+
+        def set_pixel(x, y, z, c):
+            if zbuf[y, x] >= z:
+                return
+            zbuf[y, x] = z
+            data[y, x] = c
+
+        for x, y, z, c in zip(xx, yy, zz, cc):
+            x -= xmin
+            y -= ymin
+            set_pixel(x, y, z, c | VOX_SIDE_ZL)
+            set_pixel(x + 1, y, z, c | VOX_SIDE_ZR)
+            set_pixel(x, y + 1, z, c | VOX_SIDE_X)
+            set_pixel(x + 1, y + 1, z, c | VOX_SIDE_Y)
+            set_pixel(x, y + 2, z, c | VOX_SIDE_X)
+            set_pixel(x + 1, y + 2, z, c | VOX_SIDE_Y)
+
+        self.data = data
+
+        def make_remap(func):
+            if func is None:
+                return np.arange(256)
+            return PaletteRemap.from_function(func).remap
+
+        REMAP_X = make_remap(self.x_colour_func)
+        REMAP_Y = make_remap(self.y_colour_func)
+        REMAP_Z = make_remap(self.z_colour_func)
+        self.palette = np.concatenate((REMAP_Z, REMAP_Z, REMAP_X, REMAP_Y)).astype(np.uint8)
+
+    def make_house_sprite(self, zoom, **kw):
+        if self.data is None:
+            self._load()
+
+        if zoom == ZOOM_4X:
+            data = self.palette[self.data[::2, :]]
+        elif zoom == ZOOM_2X:
+            data = self.palette[np.repeat(self.data, repeats=2, axis=1)]
+        elif zoom == ZOOM_NORMAL:
+            data = np.zeros((self.data.shape[0] * 2 + 1, self.data.shape[1] * 4), dtype=np.uint8)
+            SIDE_PIXELS = (
+                (                (2, 0), (3, 0),  # Z left
+                 (0, 1), (1, 1), (2, 1), (3, 1),
+                                 (2, 2), (3, 2)),
+                ((0, 0), (1, 0), # Z right
+                 (0, 1), (1, 1), (2, 1), (3, 1),
+                 (0, 2), (1, 2)),
+                ((0, 0), (1, 0),  # X (default)
+                 (0, 1), (1, 1), (2, 1), (3, 1),
+                                 (2, 2), (3, 2)),
+                (                (2, 0), (3, 0),  # Y (default)
+                 (0, 1), (1, 1), (2, 1), (3, 1),
+                 (0, 2), (1, 2)),
+            )
+            # Another pattern
+            # SIDE_PIXELS = (
+            #     (                        (3, 0),  # Z left
+            #              (1, 1), (2, 1), (3, 1),
+            #              (1, 2), (2, 2), (3, 2),
+            #                              (3, 3)),
+            #     ((0, 0),  # Z right
+            #      (0, 1), (1, 1), (2, 1),
+            #      (0, 2), (1, 2), (2, 2),
+            #      (0, 3)),
+            #     ((0, 0),  # X
+            #      (0, 1), (1, 1), (2, 1),
+            #              (1, 2), (2, 2), (3, 2),
+            #                              (3, 3)),
+            #     (                        (3, 0),  # Y
+            #              (1, 1), (2, 1), (3, 1),
+            #      (0, 2), (1, 2), (2, 2),
+            #      (0, 3)),
+            # )
+            for y in range(self.h):
+                for x in range(self.w):
+                    yy = 2 * y
+                    xx = 4 * x
+                    v = self.data[y, x]
+                    colour = self.palette[v]
+                    side = v >> VOX_SIDE_SHIFT
+
+                    if v & VOX_SIDE_BOTTOM > 0:
+                        # If there is same Z side below use triangle pattern
+                        if (y + 1 < self.h and self.data[y + 1, x] and
+                            self.data[y + 1, x] & VOX_SIDE_BOTTOM == 0 and
+                            self.data[y + 1, x] & VOX_SIDE_RIGHT == v & VOX_SIDE_RIGHT):
+                            # SIDE_X -> SIDE_ZR   SIDE_Y -> SIDE_ZL
+                            side ^= ((VOX_SIDE_BOTTOM | VOX_SIDE_RIGHT) >> VOX_SIDE_SHIFT)
+
+                        # If X is below Y or vice versa use triangle pattern
+                        if (y > 0 and self.data[y - 1, x] and
+                            self.data[y - 1, x] & VOX_SIDE_BOTTOM > 0 and
+                            self.data[y - 1, x] & VOX_SIDE_RIGHT != v & VOX_SIDE_RIGHT):
+                            # SIDE_X -> SIDE_ZL   SIDE_Y -> SIDE_ZR
+                            side ^= VOX_SIDE_BOTTOM >> VOX_SIDE_SHIFT
+
+                    for xo, yo in SIDE_PIXELS[side]:
+                        data[yy + yo, xx + xo] = colour
+        else:
+            raise ValueError(f'Requested unsuported vox rendering zoom: {zoom}')
+
+        im = Image.fromarray(data, mode='P')
+        im.putpalette(PALETTE)
+        return ImageSprite(im, 0, 0, data.shape[1], data.shape[0], zoom=zoom, **kw)
