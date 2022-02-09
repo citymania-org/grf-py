6b463781353c54bdd9b84b2ada32fd14635bcdcb
dP <dp@dpointer.org>
2022-02-09 23:40:24 +0300

    Add requirements

--- setup.py
+++ setup.py
@@ -2,11 +2,21 @@
 
 from distutils.core import setup
 
+
+install_requires = [
+    'nml==0.5.3.post20210524+g09de523',
+    'numpy==1.20.3',
+    'Pillow==8.2.0',
+    'spectra==0.0.11',
+]
+
 setup(
     name='grf',
     version='0.1',
     description='Framework for making OpenTTD NewGRF files',
     author='dP',
     packages=['grf'],
-    scripts=['bin/grftopy']
+    scripts=['bin/grftopy'],
+    install_requires=install_requires,
+    python_requires=">=3.8",
 )
