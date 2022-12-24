f92e87829e47ab429b240a6160facc51082b8a8d
Pavel Stupnikov <dp@dpointer.org>
2022-12-24 17:30:49 +0400

    Export StringRef, open_image and find_best_color

--- grf/__init__.py
+++ grf/__init__.py
@@ -5,9 +5,11 @@ from . import parser
 from .grf import *
 from .lib import *
 from .vox import VoxReader, VoxFile
-from .sprites import RealSprite, GraphicsSprite, ImageSprite, ImageFile, FileSprite, SoundSprite, RAWSound, PaletteRemap, EMPTY_SPRITE
+from .sprites import RealSprite, GraphicsSprite, ImageSprite, ImageFile, FileSprite, SoundSprite, RAWSound, PaletteRemap, EMPTY_SPRITE, open_image, find_best_color
 from .common import Feature, TRAIN, RV, SHIP, AIRCRAFT, STATION, RIVER, CANAL, BRIDGE, HOUSE, GLOBAL_VAR, \
                     INDUSTRY_TILE, INDUSTRY, CARGO, SOUND_EFFECT, AIRPORT, SIGNAL, OBJECT, RAILTYPE, \
                     AIRPORT_TILE, ROADTYPE, TRAMTYPE, ZOOM_4X, ZOOM_NORMAL, ZOOM_2X, ZOOM_8X, ZOOM_16X, ZOOM_32X, \
                     BPP_8, BPP_24, BPP_32, VEHICLE_FEATURES
 from .actions import RVFlags, TrainFlags, CargoClass, train_hpi, train_ton, nml_te
+from .strings import StringRef
+
