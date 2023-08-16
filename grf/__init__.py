3e669cbc0d28d328d54468a093262a57dfb6b61b
dP <dp@dpointer.org>
2023-08-17 01:08:42 +0400

    Export combine_sprite_hash function

--- grf/__init__.py
+++ grf/__init__.py
@@ -5,7 +5,7 @@ from . import parser
 from .grf import *
 from .constants import *
 from .vox import VoxReader, VoxFile, VoxTrainFile
-from .sprites import RealSprite, GraphicsSprite, ImageSprite, ImageFile, FileSprite, Mask, FileMask, ImageMask, SoundSprite, RAWSound, PaletteRemap, EMPTY_SPRITE, open_image, find_best_color, fix_palette
+from .sprites import RealSprite, GraphicsSprite, ImageSprite, ImageFile, FileSprite, Mask, FileMask, ImageMask, SoundSprite, RAWSound, PaletteRemap, EMPTY_SPRITE, open_image, find_best_color, fix_palette, combine_sprite_hash
 from .common import *
 from .actions import RVFlags, TrainFlags, CargoClass, train_hpi, train_ton, \
     nml_te, nml_drag, py_property, SpriteRef, SpriteLayout, SpriteLayoutList, \
