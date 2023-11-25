2ab75079a7ef78a0c0d975cbd68b59394f7154ef
Yi Yang @ Anteros <ahyangyi@gmail.com>
2023-11-25 14:05:06 +0800

    (typo) Check npmask instead of npalpha

--- grf/sprites.py
+++ grf/sprites.py
@@ -513,7 +513,7 @@ class Sprite(Resource):
             rbpp += 1
             stack.append(npalpha.reshape((npalpha.shape[0] * npalpha.shape[1], 1)))
         if npmask is not None:
-            if npalpha.ndim != 2:
+            if npmask.ndim != 2:
                 raise RuntimeError('Mask layer is not a 2-dimensional array')
             info_byte |= 0x4
             rbpp += 1
