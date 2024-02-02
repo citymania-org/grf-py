0fe48ae7c55ba94e4417bc6d84a2306f7cbf404b
dP <dp@dpointer.org>
2024-02-03 00:20:37 +0530

    misc: Improved house variations sheet generator

--- misc/make_house_list/make_house_list.py
+++ misc/make_house_list/make_house_list.py
@@ -13,8 +13,12 @@ FONT_SIZE = 20
 LINE_HEIGHT = FONT_SIZE + 2
 CLIMATE_COLOURS = ((0x74, 0xD5, 0x74), (0x68, 0x92, 0xB4), (0xff, 0xce, 0x8c), (0xff, 0x8c, 0x8c))
 OGFX_BASE_PATH = 'ogfx1_base.grf'
-# OGFX_BASE_PATH = 'ogfx21_base_8.grf'
+OGFX_BASE_PATH = 'ogfx21_base_8.grf'
 # OGFX_BASE_PATH = 'trg1r.grf'
+# OGFX_BASE_PATH = 'TRG1.GRF'
+BASE_OPTS = {'real_offset': 0, 'pseudo_offset': 1} if OGFX_BASE_PATH[:3].upper() == 'TRG' else {}
+
+REPLACEMENT_GRF = '../../../BonkyGFX/bonkygfx.grf'
 
 font = ImageFont.truetype('OpenTTD-Sans.ttf', FONT_SIZE)
 font_small = ImageFont.truetype('OpenTTD-Sans.ttf', FONT_SIZE // 2)
@@ -64,20 +68,23 @@ class RecolourSprite(grf.Sprite):
         assert bpp == 8
         data = np.array(im)
         data = self.recolour[data]
+        # data = np.array(grf.WIN_TO_DOS)[data]
         h, w = data.shape
-       	rgba = np.zeros((h, w, 4), np.uint8)
-       	rgba[:, :, 3] = 255
-       	for y in range(h):
-       		for x in range(w):
-       			rgba[y, x, :3] = grf.PALETTE[data[y, x]]
-       	rgba[data == 0, 3] = 0
+        rgba = np.zeros((h, w, 4), np.uint8)
+        rgba[:, :, 3] = 255
+        for y in range(h):
+            for x in range(w):
+                rgba[y, x, :3] = grf.PALETTE[data[y, x]]
+        rgba[data == 0, 3] = 0
         return Image.fromarray(rgba)
 
 
 class GRFFile(grf.LoadedResourceFile):
-    def __init__(self, path, *, baseset=False):
+    def __init__(self, path, *, baseset=False, real_offset=1, pseudo_offset=0):
         self.path = path
         self.real_sprites = None
+        self.real_offset = real_offset
+        self.pseudo_offset = pseudo_offset
         self.load(baseset)
 
     def load(self, baseset):
@@ -96,7 +103,7 @@ class GRFFile(grf.LoadedResourceFile):
         self.f.close()
 
     def get_sprite_data(self, sprite_id, num):
-        s = self.real_sprites[sprite_id + 1][num]
+        s = self.real_sprites[sprite_id + self.real_offset][num]
         self.f.seek(s.offset)
         data, _ = grf.decompile.decode_sprite(self.f, s, self.container)
         return data
@@ -104,13 +111,13 @@ class GRFFile(grf.LoadedResourceFile):
     def get_sprites(self, sprite_id):
         return [
             GRFSprite(self, s.type, s.bpp, sprite_id, i, w=s.width, h=s.height, xofs=s.xofs, yofs=s.yofs, zoom=s.zoom, crop=False)
-            for i, s in enumerate(self.real_sprites[sprite_id + 1])
+            for i, s in enumerate(self.real_sprites[sprite_id + self.real_offset])
         ]
 
     def get_recolour_sprite(self, sprite_id):
-    	s = self.pseudo_sprites[sprite_id]
-    	assert isinstance(s, grf.decompile.RealRemapSprite)
-    	return np.frombuffer(s.data[1:], dtype=np.uint8)
+        s = self.pseudo_sprites[sprite_id + self.pseudo_offset]
+        assert isinstance(s, grf.decompile.RealRemapSprite)
+        return np.frombuffer(s.data[1:], dtype=np.uint8)
 
 
 class GRFSprite(grf.Sprite):
@@ -193,83 +200,86 @@ class GRFSprite(grf.Sprite):
 # HZ_CLIMALL           = 0xF800,  ///< Bitmask of all climate bits
 
 def str_zones(availability):
-	res = []
-	for i in range(5):
-		if availability & (1 << i) > 0:
-			res.append(str(i))
-	return ', '.join(res)
+    res = []
+    for i in range(5):
+        if availability & (1 << i) > 0:
+            res.append(str(i))
+    return ', '.join(res)
 
 # def str_availability(f):
-# 	if f == ALL_CLIMATES: return 'ALL'
-# 	res = []
-# 	for i, c in enumerate(('T', 'A', 'S', 'L'))
+#   if f == ALL_CLIMATES: return 'ALL'
+#   res = []
+#   for i, c in enumerate(('T', 'A', 'S', 'L'))
 
 PALETTES = {
-	775: 'DARK_BLUE',
-	776: 'PALE_GREEN',
-	777: 'PINK',
-	778: 'YELLOW',
-	779: 'RED',
-	780: 'LIGHT_BLUE',
-	781: 'GREEN',
-	782: 'DARK_GREEN',
-	783: 'BLUE',
-	784: 'CREAM',
-	785: 'MAUVE',
-	786: 'PURPLE',
-	787: 'ORANGE',
-	788: 'BROWN',
-	789: 'GREY',
-	790: 'WHITE',
-	791: 'BARE_LAND',
-	795: 'STRUCT_BLUE',
-	796: 'STRUCT_BROWN',
-	797: 'STRUCT_WHITE',
-	798: 'STRUCT_RED',
-	799: 'STRUCT_GREEN',
-	800: 'STRUCT_CONCRETE',
-	801: 'STRUCT_YELLOW',
-	802: 'TRANSPARENT',
-	1438: 'CHURCH_RED',
-	1439: 'CHURCH_CREAM',
+    775: 'DARK_BLUE',
+    776: 'PALE_GREEN',
+    777: 'PINK',
+    778: 'YELLOW',
+    779: 'RED',
+    780: 'LIGHT_BLUE',
+    781: 'GREEN',
+    782: 'DARK_GREEN',
+    783: 'BLUE',
+    784: 'CREAM',
+    785: 'MAUVE',
+    786: 'PURPLE',
+    787: 'ORANGE',
+    788: 'BROWN',
+    789: 'GREY',
+    790: 'WHITE',
+    791: 'BARE_LAND',
+    795: 'STRUCT_BLUE',
+    796: 'STRUCT_BROWN',
+    797: 'STRUCT_WHITE',
+    798: 'STRUCT_RED',
+    799: 'STRUCT_GREEN',
+    800: 'STRUCT_CONCRETE',
+    801: 'STRUCT_YELLOW',
+    802: 'TRANSPARENT',
+    1438: 'CHURCH_RED',
+    1439: 'CHURCH_CREAM',
 }
 
 houses = []
 num_variations = 0
+used_palettes = set()
 for i, h in enumerate(data['house_specs']):
-	if not h["enabled"]:
-		continue
-	min_year, max_year = h["min_year"], h["max_year"]
-	min_year_str = str(min_year) if min_year > 0 else '*'
-	max_year_str = str(max_year) if max_year < 5000000 else '*'
-	years_str = min_year_str + '-' + max_year_str
-	zones = str_zones(h["availability"])
-	bbset = set((dt[(i << 4) + j]["subtile_x"], dt[(i << 4) + j]["subtile_y"]) for j in range(16))
-	assert len(bbset) == 1
-	bb = next(iter(bbset))
-	print(f'#{i}: {years_str} {h["name"]} Zones: {zones} BB: {bb}')
+    if not h["enabled"]:
+        continue
+    min_year, max_year = h["min_year"], h["max_year"]
+    min_year_str = str(min_year) if min_year > 0 else '*'
+    max_year_str = str(max_year) if max_year < 5000000 else '*'
+    years_str = min_year_str + '-' + max_year_str
+    zones = str_zones(h["availability"])
+    bbset = set((dt[(i << 4) + j]["subtile_x"], dt[(i << 4) + j]["subtile_y"]) for j in range(16))
+    assert len(bbset) == 1
+    bb = next(iter(bbset))
+    print(f'#{i}: {years_str} {h["name"]} Zones: {zones} BB: {bb}')
 
-	variations = set()
-	for j in range(4):
-		stages = []
-		for k in range(4):
-			d = dt[(i << 4) | (j << 2) | k]
-			v = tuple(d.items())
-			if stages and stages[-1] == v:
-				continue
-			stages.append(v)
-		variations.add(tuple(stages))
-	houses.append((i, h, variations, years_str, zones, bb))
-	num_variations += len(variations)
+    variations = set()
+    for j in range(4):
+        stages = []
+        for k in range(4):
+            d = dt[(i << 4) | (j << 2) | k]
+            used_palettes.add(d["building_pal"])
+            used_palettes.add(d["ground_pal"])
+            v = tuple(d.items())
+            if stages and stages[-1] == v:
+                continue
+            stages.append(v)
+        variations.add(tuple(stages))
+    houses.append((i, h, variations, years_str, zones, bb))
+    num_variations += len(variations)
 
-	for j, v in enumerate(variations):
-		print(f'    Variation {j}:')
-		for fd in stages:
-			d = dict(fd)
-			strpal = lambda p: '' if p == 0 else ':' + PALETTES[p]
-			assert d["draw_proc"] in (0, 1)
-			liftstr = ' LIFT' if d["draw_proc"] == 1 else ''
-			print(f'        {d["ground_sprite"]}{strpal(d["ground_pal"])} {d["building_sprite"]}{strpal(d["building_pal"])} {liftstr}')
+    for j, v in enumerate(variations):
+        print(f'    Variation {j}:')
+        for fd in stages:
+            d = dict(fd)
+            strpal = lambda p: '' if p == 0 else ':' + PALETTES[p]
+            assert d["draw_proc"] in (0, 1)
+            liftstr = ' LIFT' if d["draw_proc"] == 1 else ''
+            print(f'        {d["ground_sprite"]}{strpal(d["ground_pal"])} {d["building_sprite"]}{strpal(d["building_pal"])} {liftstr}')
 
 #     j.kv("min_year", hs->min_year);
 #     j.kv("max_year", hs->max_year);
@@ -297,10 +307,12 @@ for i, h in enumerate(data['house_specs']):
 #     j.kv("draw_proc", d.draw_proc);
 
 
-ogfx = GRFFile(OGFX_BASE_PATH, baseset=True)
+ogfx = GRFFile(OGFX_BASE_PATH, baseset=True, **BASE_OPTS)
+replacement = GRFFile(REPLACEMENT_GRF, baseset=False)
+
 PALETTE_REMAPS = {}
 for k in PALETTES.keys():
-	PALETTE_REMAPS[k] = ogfx.get_recolour_sprite(k)
+    PALETTE_REMAPS[k] = ogfx.get_recolour_sprite(k)
 
 # Add fake remap that does nothing
 PALETTES[0] = ''
@@ -312,81 +324,112 @@ im = Image.new(mode="RGBA", size=(WIDTH, h), color=(255, 255, 255, 255))
 draw = ImageDraw.Draw(im)
 y = 0
 for i, h, variations, years_str, zones, bb in houses:
-	avl = h['availability']
+    avl = h['availability']
 
-	CLIMATE_SIZE = 18
-	CLIMATE_OFS = font.getlength(f'#{i}') + 5
-	cx, cy = PADDING + CLIMATE_OFS, y + PADDING - 3
-	draw.rounded_rectangle(((cx, cy), (cx + CLIMATE_SIZE * 4 + 6, cy + CLIMATE_SIZE + 3)), outline=(0, 0, 0), radius=5)
-	for j, c in enumerate(CLIMATE_COLOURS):
-		ccx, ccy = cx + 2 + (CLIMATE_SIZE + 1) * j, cy + 2
-		if j == 1:
-			if avl & 0x2800 == 0:
-				continue
+    CLIMATE_SIZE = 18
+    CLIMATE_OFS = font.getlength(f'#{i}') + 5
+    cx, cy = PADDING + CLIMATE_OFS, y + PADDING - 3
+    draw.rounded_rectangle(((cx, cy), (cx + CLIMATE_SIZE * 4 + 6, cy + CLIMATE_SIZE + 3)), outline=(0, 0, 0), radius=5)
+    for j, c in enumerate(CLIMATE_COLOURS):
+        ccx, ccy = cx + 2 + (CLIMATE_SIZE + 1) * j, cy + 2
+        if j == 1:
+            if avl & 0x2800 == 0:
+                continue
 
-			if avl & 0x800 > 0:
-				draw.rounded_rectangle(((ccx, ccy), (ccx + CLIMATE_SIZE - 1, ccy + CLIMATE_SIZE // 2 + 3 - 1)), fill=c, radius=4)
+            if avl & 0x800 > 0:
+                draw.rounded_rectangle(((ccx, ccy), (ccx + CLIMATE_SIZE - 1, ccy + CLIMATE_SIZE // 2 + 3 - 1)), fill=c, radius=4)
 
-			if avl & 0x2000 > 0:
-				draw.rounded_rectangle(((ccx, ccy + CLIMATE_SIZE // 2 - 3), (ccx + CLIMATE_SIZE - 1, ccy + CLIMATE_SIZE - 1)), fill=c, radius=4)
-		else:
-			if avl & (1 << (j + 12)) == 0:
-				continue
-			draw.rounded_rectangle(((ccx, ccy), (ccx + CLIMATE_SIZE - 1, ccy + CLIMATE_SIZE - 1)), fill=c, radius=4)
+            if avl & 0x2000 > 0:
+                draw.rounded_rectangle(((ccx, ccy + CLIMATE_SIZE // 2 - 3), (ccx + CLIMATE_SIZE - 1, ccy + CLIMATE_SIZE - 1)), fill=c, radius=4)
+        else:
+            if avl & (1 << (j + 12)) == 0:
+                continue
+            draw.rounded_rectangle(((ccx, ccy), (ccx + CLIMATE_SIZE - 1, ccy + CLIMATE_SIZE - 1)), fill=c, radius=4)
 
-	draw.text((PADDING, PADDING + y), f'#{i}', font=font, fill=(0, 0, 0))
-	draw.text((PADDING, PADDING + LINE_HEIGHT * 1 + y), f'Years: {years_str}', font=font, fill=(0, 0, 0))
-	draw.text((PADDING, PADDING + LINE_HEIGHT * 2 + y), f'Zones: {zones}', font=font, fill=(0, 0, 0))
-	draw.text((PADDING, PADDING + LINE_HEIGHT * 3 + y), f'BB: {bb}', font=font, fill=(0, 0, 0))
+    draw.text((PADDING, PADDING + y), f'#{i}', font=font, fill=(0, 0, 0))
+    draw.text((PADDING, PADDING + LINE_HEIGHT * 1 + y), f'Years: {years_str}', font=font, fill=(0, 0, 0))
+    draw.text((PADDING, PADDING + LINE_HEIGHT * 2 + y), f'Zones: {zones}', font=font, fill=(0, 0, 0))
+    draw.text((PADDING, PADDING + LINE_HEIGHT * 3 + y), f'BB: {bb}', font=font, fill=(0, 0, 0))
 
-	yb = y
-	for j, stages in enumerate(variations):
-		mh = 0
-		for k, fd in enumerate(stages):
-			d = dict(fd)
-			building_id = d["building_sprite"]
-			if building_id > 0:
-				mh = max(mh, ogfx.get_sprites(building_id)[0].h)
-		gy = y + PADDING + mh
+    yb = y
+    for j, stages in enumerate(variations):
+        mh = 0
+        for k, fd in enumerate(stages):
+            d = dict(fd)
+            building_id = d["building_sprite"]
+            if building_id > 0:
+                mh = max(mh, ogfx.get_sprites(building_id)[0].h)
+        gy = y + PADDING + mh
 
-		for k, fd in enumerate(stages):
-			d = dict(fd)
-			ground_id, building_id = d["ground_sprite"], d["building_sprite"]
-			ground_pal_id, building_pal_id = d["ground_pal"], d["building_pal"]
-			IMG_OFS = 220 + 200 * k
-			if building_id > 0:
-				building = ogfx.get_sprites(building_id)[0]
-				building = RecolourSprite(building, PALETTE_REMAPS[building_pal_id])
-				building_img = building.get_rgba_image()
-				w, h = building_img.size
-				im.alpha_composite(building_img, (IMG_OFS + building.xofs + 31, gy - h))
-				draw.text((IMG_OFS + 64 + PADDING, gy - h),
-						  f'{building_id}',
-						  font=font_small, fill=(0, 0, 0))
-				draw.text((IMG_OFS + 64 + PADDING, gy - h + LINE_HEIGHT // 2),
-						  PALETTES[building_pal_id],
-						  font=font_small, fill=(0, 0, 0))
-			if d["draw_proc"] == 1:
-				draw.text((IMG_OFS + 64 + PADDING, gy - h + LINE_HEIGHT),
-						  'LIFT',
-						  font=font_small, fill=(0, 0, 0))
+        for k, fd in enumerate(stages):
+            d = dict(fd)
+            ground_id, building_id = d["ground_sprite"], d["building_sprite"]
+            ground_pal_id, building_pal_id = d["ground_pal"], d["building_pal"]
+            IMG_OFS = 220 + 200 * k
+            if building_id > 0:
+                building = ogfx.get_sprites(building_id)[0]
+                building = RecolourSprite(building, PALETTE_REMAPS[building_pal_id])
+                building_img = building.get_rgba_image()
+                w, h = building_img.size
+                im.alpha_composite(building_img, (IMG_OFS + building.xofs + 31, gy - h))
+                draw.text((IMG_OFS + 64 + PADDING, gy - h),
+                          f'{building_id}',
+                          font=font_small, fill=(0, 0, 0))
+                draw.text((IMG_OFS + 64 + PADDING, gy - h + LINE_HEIGHT // 2),
+                          PALETTES[building_pal_id],
+                          font=font_small, fill=(0, 0, 0))
+            if d["draw_proc"] == 1:
+                draw.text((IMG_OFS + 64 + PADDING, gy - h + LINE_HEIGHT),
+                          'LIFT',
+                          font=font_small, fill=(0, 0, 0))
 
-			if ground_id > 0:
-				ground = ogfx.get_sprites(ground_id)[0]
-				ground = RecolourSprite(ground, PALETTE_REMAPS[ground_pal_id])
-				ground_img = ground.get_rgba_image()
-				w, h = ground_img.size
-				im.alpha_composite(ground_img, (IMG_OFS, gy + 1))
-				draw.text((IMG_OFS + 64 + PADDING, gy + 33 - LINE_HEIGHT // 2), str(ground_id), font=font_small, fill=(0, 0, 0))
-				draw.text((IMG_OFS + 64 + PADDING, gy + 33 - LINE_HEIGHT), PALETTES[ground_pal_id], font=font_small, fill=(0, 0, 0))
+            if ground_id > 0:
+                ground = ogfx.get_sprites(ground_id)[0]
+                ground = RecolourSprite(ground, PALETTE_REMAPS[ground_pal_id])
+                ground_img = ground.get_rgba_image()
+                w, h = ground_img.size
+                im.alpha_composite(ground_img, (IMG_OFS, gy + 1))
+                draw.text((IMG_OFS + 64 + PADDING, gy + 33 - LINE_HEIGHT // 2), str(ground_id), font=font_small, fill=(0, 0, 0))
+                draw.text((IMG_OFS + 64 + PADDING, gy + 33 - LINE_HEIGHT), PALETTES[ground_pal_id], font=font_small, fill=(0, 0, 0))
 
-		y = gy + 33
-	y += PADDING
-	if y - yb < 100:
-		y = yb + 100
-	draw.line(((0, y), (WIDTH - 1, y)), fill=(0, 0, 0))
-	y += 1
+        y = gy + 33
+    y += PADDING
+    if y - yb < 100:
+        y = yb + 100
+    draw.line(((0, y), (WIDTH - 1, y)), fill=(0, 0, 0))
+    y += 1
 
 im = im.crop((0, 0, WIDTH, y))
-im.show()
+im.save('house_variations.png')
 
+print('Palettes used:', ', '.join(v for k, v in PALETTES.items() if k in used_palettes))
+# for i in 795, 796, 797, 798, 799, 800, 801:
+    # remap = [PALETTE_REMAPS[i][j] for j in range(70, 80)]
+    # print(f'REMAP {i}: {remap}')
+for i in 775, 776, 777, 778, 779, 780, 781, 782, 783, 784, 785, 786, 787, 788, 789, 790:
+    remap = [PALETTE_REMAPS[i][j] for j in range(198, 206)]
+    print(f'REMAP {i}: {remap}')
+# for i in PALETTES.keys():
+#     print(f'REMAP {i}:')
+#     for j in range(256):
+#       m = PALETTE_REMAPS[i][j]
+#       if m != j:
+#           print(f'    {j}->{m}')
+
+# ogfx 1&2
+# 795: [144, 144, 145, 146, 147, 148, 149, 150, 151, 152]
+# 796: [104, 105, 106, 107, 108, 109, 110, 111, 37, 38]
+# 797: [70, 3, 5, 7, 8, 10, 11, 12, 13, 14]
+# 798: [70, 178, 179, 180, 181, 162, 163, 164, 165, 166]
+# 799: [70, 96, 96, 97, 98, 99, 100, 101, 102, 103]
+# 800: [70, 16, 17, 18, 19, 20, 21, 22, 23, 14]
+# 801: [60, 61, 62, 63, 64, 65, 66, 67, 68, 69]
+
+# original_win
+# REMAP 795: [144, 144, 145, 146, 147, 148, 149, 150, 151, 152]
+# REMAP 796: [104, 105, 53, 107, 108, 109, 110, 111, 37, 38]
+# REMAP 797: [70, 136, 106, 33, 40, 10, 11, 12, 13, 14]
+# REMAP 798: [70, 178, 179, 180, 181, 162, 163, 164, 165, 166]
+# REMAP 799: [70, 96, 96, 97, 98, 99, 100, 101, 102, 103]
+# REMAP 800: [70, 16, 17, 18, 19, 20, 21, 22, 23, 14]
+# REMAP 801: [60, 61, 62, 63, 64, 65, 66, 67, 68, 69]
