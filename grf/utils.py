8831073348578ae2bdaa05af930dfa5adf450258
dP <dp@dpointer.org>
2024-03-08 23:15:50 +0700

    Add --live-reload argument to watch command to specify the admin port connection string

--- grf/utils.py
+++ grf/utils.py
@@ -4,6 +4,9 @@ import time
 import os
 import subprocess
 import signal
+import traceback
+from concurrent.futures.thread import ThreadPoolExecutor
+from pathlib import Path
 
 from .grf import BaseNewGRF
 
@@ -15,9 +18,6 @@ import struct
 
 from pprint import pprint
 
-SERVER = 'localhost'
-PORT = 3977
-PASSWORD = b'123'
 ADMIN_NAME = 'grf-py live reload'
 ADMIN_VERSION = '1.0'
 
@@ -47,12 +47,31 @@ def make_packet(packet_type, fmt=None, *args):
     return bytes((size & 255, size >> 8, packet_type)) + data
 
 
-async def reload_newgrfs():
+async def admin_logging(queue, admin_addr):
+    if admin_addr is None:
+        return
+
+    server, port, password = admin_addr
+
     reader, writer = await asyncio.wait_for(
-        asyncio.open_connection(SERVER, PORT),
+        asyncio.open_connection(server, port),
         timeout=3)
 
-    writer.write(make_packet(ADMIN_JOIN, 'zzz', PASSWORD, ADMIN_NAME, ADMIN_VERSION))
+    writer.write(make_packet(ADMIN_JOIN, 'zzz', password, ADMIN_NAME, ADMIN_VERSION))
+
+    prev_msg = ''
+    while True:
+        msg = await queue.get()
+        if msg is None:
+            queue.task_done()
+            break
+        if msg.endswith('\n'):
+            writer.write(make_packet(ADMIN_EXTERNAL_CHAT, 'zHzz', 'grf-py', 1, '', prev_msg + msg.rstrip('\n')))
+            prev_msg = ''
+        else:
+            prev_msg += msg
+        queue.task_done()
+
     writer.write(make_packet(ADMIN_RCON, 'z', 'reload_newgrfs'))
     await writer.drain()
     writer.close()
@@ -68,9 +87,50 @@ def build_func(g, grf_file, args):
     )
 
 
+async def async_compile(g, grf_file, queue, admin_addr):
+    loop = asyncio.get_running_loop()
+    executor = ThreadPoolExecutor(max_workers=1)
+
+    def compile_func(g, grf_file):
+        watched = g.write(grf_file)
+        queue.put_nowait(None)
+        g._context.print(f'Reloading newgrfs.')
+        return watched
+
+    compilation = loop.run_in_executor(executor, compile_func, g, grf_file)
+    logging = admin_logging(queue, admin_addr)
+    group = asyncio.gather(compilation, logging)
+
+    try:
+        res = await group
+        return res[0]
+    except BaseException as e:
+        group.cancel()
+        tb = traceback.format_exc()
+        for l in tb.split('\n'):
+            g._context.print(l)
+        return None
+
+
 def watch_func(g, grf_file, args):
     import watchdog.events
     import watchdog.observers
+
+    admin_addr = None
+    if args.live_reload is not None:
+        s = args.live_reload
+        port = 3977
+        addr = '127.0.0.1'
+        if ':' in s:
+            s, port_str = s.rsplit(':', 1)
+            port = int(port_str)
+        if '@' in s:
+            s, addr = s.split('@', 1)
+        pw = s
+        print(f'Admin port connection string: {pw}@{addr}:{port}')
+        admin_addr = (addr, port, pw)
+    else:
+        print(f'No admin port specified, live reload unavailable')
 
     class FileChangeHandler(watchdog.events.FileSystemEventHandler):
         def __init__(self, file_list):
@@ -81,45 +141,59 @@ def watch_func(g, grf_file, args):
             self.modified = set()
 
         def on_modified(self, event):
-            if event.src_path in self.file_list:
+            if self.file_list is None:
+                return
+            if Path(event.src_path).resolve() in self.file_list:
                 self.modified.add(event.src_path)
 
-    print(f'Building {grf_file}')
-    watched_files = g.write(grf_file)
+    queue = None
 
-    # print(f'Starting OpenTTD.')
-    # process = subprocess.Popen([OPENTTD], stdin=subprocess.PIPE, stdout=subprocess.PIPE, text=True, shell=False)
-    asyncio.run(reload_newgrfs())
+    def print_func(msg, end='\n', flush=False):
+        nonlocal queue
+        if queue is None:
+            return
+        queue.put_nowait(msg + end)
 
-    print(f'Watching {len(watched_files)} files for changes...')
+    g._context.add_print_handler(print_func)
 
-    event_handler = FileChangeHandler(watched_files)
-    observer = watchdog.observers.Observer()
-
-    for file_path in watched_files:
-        observer.schedule(event_handler, os.path.dirname(file_path), recursive=False)
-
-    observer.start()
-
-    try:
-        while True:
-            time.sleep(1)
-            if event_handler.modified:
+    async def run_compile():
+        event_handler = FileChangeHandler(None)
+        observer = watchdog.observers.Observer()
+        observer.start()
+        nonlocal queue
+        try:
+            while True:
                 time.sleep(1)
-                modified_files = ', '.join(event_handler.modified)
-                print(f"{modified_files} has been modified, rebuilding grf")
-                event_handler.reset()
-                g.write(grf_file)
-                print(f'Reloading newgrfs.')
-                asyncio.run(reload_newgrfs())
-                # process.stdin.write('reload_newgrfs\n')
-                # output = process.stdout.read()
-                # print('OUT', output)
-    except KeyboardInterrupt:
-        observer.stop()
+                if event_handler.modified or event_handler.file_list is None:
+                    time.sleep(1)
+                    modified_files = ', '.join(event_handler.modified)
+                    event_handler.reset()
+                    queue = asyncio.Queue()
+                    if not modified_files:
+                        g._context.print(f"Building {grf_file}")
+                    else:
+                        g._context.print(f"{modified_files} has been modified, rebuilding {grf_file}")
+                    prev_watched = event_handler.file_list
+                    watched_files = await async_compile(g, grf_file, queue, admin_addr)
+                    if watched_files is None:
+                        if prev_watched is None:
+                            g._context.print(f'Grf build failed, retrying in 10 seconds...')
+                            time.sleep(10)
+                    else:
+                        event_handler.file_list = {Path(f).resolve() for f in watched_files}
+                        if prev_watched != watched_files:
+                            g._context.print(f'Watching {len(watched_files)} files for changes...')
+                        for file_path in watched_files:
+                            if prev_watched is not None and file_path in prev_watched:
+                                continue
+                            observer.schedule(event_handler, os.path.dirname(file_path), recursive=False)
 
+        except KeyboardInterrupt:
+            observer.stop()
+        observer.join()
+
+    asyncio.run(run_compile())
     # process.send_signal(signal.SIGKILL)
-    observer.join()
 
 
 def init_id_map_func(g, grf_file, args):
@@ -146,6 +220,7 @@ def main(g, grf_file, commands=None):
     # Create a parser for the 'build' command
     watch_parser = subparsers.add_parser('watch', help='Build newgrf and rebuild if any files changed')
     watch_parser.set_defaults(func=watch_func)
+    watch_parser.add_argument('--live-reload', type=str, help='Admin port to connect in a form password@address:port')
 
     watch_parser = subparsers.add_parser('init_id_map', help='Initialize the automatic id index (id_map.json)')
     watch_parser.set_defaults(func=init_id_map_func)
